fd36b23d1e7d444adea18d57c5443778e8f13964
Valentin LE MECHEC <valentin.lemechec@gmail.com>
2017-12-11 15:42:17 +0100

    debut

---
+++ src/main/java/Personne.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Personne {
+
+	
+	private Boolean estUnClient;
+	private String nom;
+	private String prenom;
+	private Date dateDeNaissance;
+	
+	
+	public Boolean estClient() {
+		
+	}
+	
+	
+}
